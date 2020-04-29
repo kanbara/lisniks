0c9dbbf1bb12c62f710c9553f6fd40b6f4d13542
Jay Morrow <jay@moia.io>
2020-04-29 02:43:04 +0200

    add wonderful word wrapping

--- pkg/ui/definition.go
+++ pkg/ui/definition.go
@@ -3,9 +3,12 @@ package ui
 import (
 	"fmt"
 	"github.com/awesome-gocui/gocui"
+	"strings"
 )
 
 type DefinitionView NoBindingsView
+
+const spaceWidth = 1
 
 func (d *DefinitionView) New(g *gocui.Gui, name string) error {
 
@@ -27,15 +30,42 @@ func (d *DefinitionView) New(g *gocui.Gui, name string) error {
 	return nil
 }
 
+// TODO at some point if we allow editing here,
+// we have to save the word-break state as a `view` state and not just
+// modify the word!
+// esp because we get rid of all that weird html and stuff
 func (d *DefinitionView) Update(v *gocui.View) error {
 	v.Clear()
 
+	x, _ := v.Size()
 	if d.state.CurrentWord() != nil {
-		_, err := fmt.Fprintln(v, d.state.CurrentWord().Def)
+
+		modified := breakWordBoundaries(d.state.CurrentWord().Def.String(), x-2)
+		_, err := fmt.Fprintln(v, modified)
 		if err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func breakWordBoundaries(word string, lineWidth int) string {
+	//shamelessely lifted this from the stupid internet and implemented it here
+	spaceLeft := lineWidth
+	out := make([]string, 0, len(word))
+
+	words := strings.Split(word, " ")
+	for _, w := range words {
+		if len([]rune(w)) + spaceWidth > spaceLeft {
+			out = append(out, "\n")
+			out = append(out, w)
+			spaceLeft = lineWidth - len([]rune(w))
+		} else {
+			out = append(out, w)
+			spaceLeft = spaceLeft - (len([]rune(w)) + spaceWidth)
+		}
+	}
+
+	return strings.Join(out, " ")
 }
